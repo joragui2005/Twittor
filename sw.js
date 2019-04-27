9c2d2c7e7ba0bc39569abc0595eff063efcef3a8
George <jorge.bravo77@gmail.com>
2019-04-27 11:05:11 -0500

    rutas
    ok

--- sw.js
+++ sw.js
@@ -6,24 +6,24 @@ const DINAMYC_CACHE = "dinamyc-v1";
 const INMMUTABLE_CACHE = "inmmutable-v1";
 
 const APP_SHELL = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/img/favicon.ico",
-  "/img/avatars/hulk.jpg",
-  "/img/avatars/ironman.jpg",
-  "/img/avatars/spiderman.jpg",
-  "/img/avatars/thor.jpg",
-  "/img/avatars/wolverine.jpg",
-  "/js/app.js",
-  "/js/sw-utils.js"
+  //"/",
+  "index.html",
+  "css/style.css",
+  "img/favicon.ico",
+  "img/avatars/hulk.jpg",
+  "img/avatars/ironman.jpg",
+  "img/avatars/spiderman.jpg",
+  "img/avatars/thor.jpg",
+  "img/avatars/wolverine.jpg",
+  "js/app.js",
+  "js/sw-utils.js"
 ];
 const APP_SHELL_INMMUTABLE = [
   "https://fonts.googleapis.com/css?family=Quicksand:300,400",
   "https://fonts.googleapis.com/css?family=Lato:400,300",
   "https://use.fontawesome.com/releases/v5.3.1/css/all.css",
-  "/css/animate.css",
-  "/js/libs/jquery.js"
+  "css/animate.css",
+  "js/libs/jquery.js"
 ];
 
 //Instalación del SW
